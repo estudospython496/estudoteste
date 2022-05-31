959b1f99dbc6c4dd4cd4ce244e848464e356b440
Bruno Duarte <57137017+duartebruno496@users.noreply.github.com>
2022-05-31 08:44:45 -0300

    estudos

---
+++ estudos.py
@@ -0,0 +1,27 @@
+print("ola")
+print('ola')
+
+w = "Curso de Python3"
+print(w)
+
+a = """ ola,
+este é o curso de python
+espero que goste"""
+
+print(a)
+
+#Métodos das Strings
+
+b = " Ola Mundo "
+print(b.strip())
+
+c = "Ola Mundo"
+print(c.lower())
+
+d = "ola mundo"
+print(d.upper())
+
+e  = "Ola"
+f = "Mundo"
+g = e + " " + f
+print(g)
